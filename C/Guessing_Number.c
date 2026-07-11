6fae8016164606fef57d18277f8e521fd5818fd2
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-11 17:24:09 +0530

    Updated

--- C/Guessing_Number.c
+++ C/Guessing_Number.c
@@ -3,7 +3,28 @@
 #include<time.h>
 
 int main(){
-    
+    int random, guess, attempts = 0;
+
+    srand(time(0));
+    random = rand() % 100 + 1;
+
+    printf("\nWelcome to the Guessing Game!\n");
+    printf("I have selected a number between 1 and 100. Can you guess it?\n");
+
+    do {
+        printf("\nEnter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess < random) {
+            printf("Too low! Try again.\n");
+        } else if (guess > random) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You've guessed the number %d in %d attempts.\n\n", random, attempts);
+        }
+
+    } while (guess != random);
 
     return 0;
 }
