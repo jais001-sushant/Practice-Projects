f5d9e812f29f873eb90116649844667c42e9e121
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-13 14:10:00 +0530

    Updated

--- C/Simple_Calculator.c
+++ C/Simple_Calculator.c
@@ -2,7 +2,7 @@
 #include<math.h>
 
 void choice_operation(int choice, int num1, int num2){
-    printf("---------------------------------\n");
+    printf("--------------------------------\n");
     printf("\nWelcome to the Simple Calculator\n");
     printf("\nSelect an operation:\n");
     printf("1. Addition\n");
@@ -12,7 +12,7 @@ void choice_operation(int choice, int num1, int num2){
     printf("5. Modulus\n");
     printf("6. Power\n");
     printf("7. Exit\n");
-    printf("Enter your choice: ");
+    printf("Enter your choice (1-7): ");
 }
 
 int main(){
@@ -21,48 +21,74 @@ int main(){
 
     while(1) {
         choice_operation(choice, num1, num2);
-        scanf("%d", &choice);
+        if(scanf("%d", &choice) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            while(getchar() != '\n');
+            continue;
+        }
 
         if (choice == 7) {
             printf("Exiting the calculator. Goodbye!\n");
             break;
         }
-        else if (choice < 1 || choice > 7){
-            printf("Invalid choice. Please select a valid option.\n");
+        else if (choice < 1 || choice > 7) {
+            printf("Invalid choice! Please select a valid operation.\n");
             continue;
         }
 
-        printf("Enter first number: ");
-        scanf("%lf", &num1);
+        printf("\nEnter first number: ");
+        if(scanf("%lf", &num1) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            while(getchar() != '\n');
+            continue;
+        }
         printf("Enter second number: ");
-        scanf("%lf", &num2);
+        if(scanf("%lf", &num2) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            while(getchar() != '\n');
+            continue;
+        }
 
         switch(choice){
             case 1:
                 result = num1 + num2;
             break;
+
             case 2:
                 result = num1 - num2;
             break;
+
             case 3:
                 result = num1 * num2;
             break;
+
             case 4:
+                if(num2 == 0){
+                    printf("Error: Division by zero is not allowed.\n");
+                    continue;
+                }
                 result = num1 / num2;
             break;
+
             case 5:
+                if(num2 == 0){
+                    printf("Error: Modulus by zero is not allowed.\n");
+                    continue;
+                }
                 result = (int)num1 % (int)num2;
             break;
+
             case 6:
                 result = pow(num1, num2);
             break;
+
             default:
-                printf("Invalid choice. Please select a valid option.\n");
+                printf("Invalid choice! Please select a valid operation.\n");
                 continue;
         }
 
-        printf("Result: %.2f\n", result);
-        printf("---------------------------------\n");
+        printf("\nResult of the operation: %.2f",result);
+        printf("\n--------------------------------\n");
     };
 
     return 0;
