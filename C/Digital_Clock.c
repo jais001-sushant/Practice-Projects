b4db5823e5f66edeff43f55c24e69c54c4347459
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-13 15:45:24 +0530

    Updated

--- C/Digital_Clock.c
+++ C/Digital_Clock.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<time.h>
 
-int main(){
+void display_time(char* buffer){
     time_t raw_time;
     struct tm *current_time;
-    char buffer[50];
 
     time(&raw_time);
     current_time = localtime(&raw_time);
-    strftime(buffer, sizeof(buffer), "%I:%M:%S %p", current_time);
+    strftime(buffer, 50, "%I:%M:%S %p", current_time);
+}
+
+int main(){
+    char buffer[50];
+    display_time(buffer);
 
     printf("Current Time: %s\n", buffer);
 
