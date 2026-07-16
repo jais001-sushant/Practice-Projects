afa339461f4bd70dda1c1d83a3d8014c44be9dd6
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-16 18:58:51 +0530

    Updated

--- C/User_Management.c
+++ C/User_Management.c
@@ -52,7 +52,7 @@ void input_credentials(char* username, char* password) {
 
 void register_user() {
     if (user_count >= MAX_USERS) {
-        printf("User limit reached. Cannot register more users.\n");
+        printf("\nUser limit reached. Cannot register more users.\n");
         return;
     }
 
@@ -60,7 +60,7 @@ void register_user() {
     input_credentials(users[user_count].username, users[user_count].password);
 
     user_count++;
-    printf("User registered successfully!\n");
+    printf("\nUser registered successfully!\n");
 }
  
 int login_user() {
@@ -95,11 +95,11 @@ int main(){
         getchar();
 
         if (option == 5) {
-            printf("Exiting the User Management System. Goodbye!\n");
+            printf("\nExiting the User Management System. Goodbye!\n\n");
             break;
         }
         else if (option < 1 || option > 5) {
-            printf("Invalid choice! Please select a valid option.\n");
+            printf("\nInvalid choice! Please select a valid option.\n");
             continue;
         }
 
@@ -111,21 +111,21 @@ int main(){
             case 2:
                 user_index = login_user();
                 if (user_index != -1) {
-                    printf("Login successful! Welcome, %s\n", users[user_index].username);
+                    printf("\nLogin successful! Welcome, %s\n", users[user_index].username);
                 } else {
-                    printf("Invalid username or password. Please try again.\n");
+                    printf("\nInvalid username or password. Please try again.\n");
                 }
             break;
 
             case 3:
-                printf("Displaying Users...\n");
+                printf("\nDisplaying Users...\n");
             break;
 
             case 4:
-                printf("Deleting User...\n");
+                printf("\nDeleting User...\n");
             break;
             default:
-                printf("Invalid choice! Please select a valid option.\n");
+                printf("\nInvalid choice! Please select a valid option.\n");
                 continue;
         }
     }
