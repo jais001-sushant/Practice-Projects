bef74998f6f6b5d62f10e19907380bfdd7029831
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-19 03:56:15 +0530

    Updated

---
+++ C/Tic-Tac-Toe.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int difficulty_level;
+
+int main() {
+    printf("\nWelcome to the tic-tac-toe game!\n\n");
+    printf("Select difficulty level:\n");
+    printf("1 - Human (Standard)\n");
+    printf("2 - God Mode (Unbeatable)\n");
+    printf("Choose an option (1 or 2): ");
+    while (scanf("%d", &difficulty_level) != 1 || (difficulty_level != 1 && difficulty_level != 2)) {
+        printf("\nInvalid input. Please enter 1 for Human or 2 for God Mode: ");
+        while(getchar() != '\n');
+    }
+
+    return 0;
+}
