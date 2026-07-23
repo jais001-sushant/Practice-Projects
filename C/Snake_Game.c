231beba9ca750dea8849c9322cd6ca9d1c1a7b26
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-24 04:40:12 +0530

    Updated

--- C/Snake_Game.c
+++ C/Snake_Game.c
@@ -11,13 +11,23 @@ void clear_screen() {
     #endif
 }
 
-int main() {
-
+void set_terminal_attributes() {
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    newt.c_lflag = ~(ICANON | ECHO);
+    newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+}
+
+void resey_terminal_attributes() {
+    struct termios oldt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    oldt.c_lflag |= (ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+}
+
+int main() {
+    set_terminal_attributes();
 
     return 0;
 }
