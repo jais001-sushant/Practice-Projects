42f2759abcd8a4f9c77e32e15a33113c2b378d6a
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-26 22:03:54 +0530

    Updated

--- C/KBC_Game.c
+++ C/KBC_Game.c
@@ -21,11 +21,34 @@ void read_questions(char* filename, Question* questions) {
 
     char str[MAX_QUES_LEN];
     int no_of_lines = 0;
-    while (fgets(str, sizeof(str), file) != NULL) {
+    while (fgets(str, MAX_QUES_LEN, file) != NULL) {
         no_of_lines++;
     }
     int no_of_questions = no_of_lines / 8;
     questions = (Question*)malloc(no_of_questions * sizeof(Question));
+
+    rewind(file);
+    for (int i = 0; i < no_of_questions; i++) {
+        fgets(questions[i].text, MAX_QUES_LEN, file);
+        
+        for (int j = 0; j < 4; j++) {
+            fgets(questions[i].options[j], MAX_OPTION_LEN, file);
+        }
+
+        char option[5];
+        fgets(option, 5, file);
+        questions[i].correct_option = option[0];
+
+        char timeout[10];
+        fgets(timeout, 10, file);
+        questions[i].timeout = atoi(timeout);
+
+        char prize_money[10];
+        fgets(prize_money, 10, file);
+        questions[i].prize_money = atoi(prize_money);
+    }
+
+    fclose(file);
 }
 
 int main() {
