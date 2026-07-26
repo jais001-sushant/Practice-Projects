934ffb2a673aca21c11e6dfca62a1e44e88d329a
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-26 21:59:59 +0530

    Updated

--- C/KBC_Game.c
+++ C/KBC_Game.c
@@ -7,6 +7,7 @@
 typedef struct {
     char text[MAX_QUES_LEN];
     char options[4][MAX_OPTION_LEN];
+    char correct_option;
     int timeout;
     int prize_money;
 } Question;
@@ -17,6 +18,14 @@ void read_questions(char* filename, Question* questions) {
         printf("File doesn't exist\n");
         exit(0);
     }
+
+    char str[MAX_QUES_LEN];
+    int no_of_lines = 0;
+    while (fgets(str, sizeof(str), file) != NULL) {
+        no_of_lines++;
+    }
+    int no_of_questions = no_of_lines / 8;
+    questions = (Question*)malloc(no_of_questions * sizeof(Question));
 }
 
 int main() {
