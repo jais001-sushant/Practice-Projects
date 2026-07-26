f471a151894ddc6b130e62c783e12316a834ade2
Sushant Jaiswal <jaiswal001sushant@gmail.com>
2026-07-26 22:40:29 +0530

    Updated

--- C/KBC_Game.c
+++ C/KBC_Game.c
@@ -12,7 +12,7 @@ typedef struct {
     int prize_money;
 } Question;
 
-void read_questions(char* filename, Question* questions) {
+int read_questions(char* filename, Question** questions) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("File doesn't exist\n");
@@ -25,35 +25,49 @@ void read_questions(char* filename, Question* questions) {
         no_of_lines++;
     }
     int no_of_questions = no_of_lines / 8;
-    questions = (Question*)malloc(no_of_questions * sizeof(Question));
-
+    *questions = (Question*)malloc(no_of_questions * sizeof(Question));
     rewind(file);
     for (int i = 0; i < no_of_questions; i++) {
-        fgets(questions[i].text, MAX_QUES_LEN, file);
+        fgets((*questions)[i].text, MAX_QUES_LEN, file);
         
         for (int j = 0; j < 4; j++) {
-            fgets(questions[i].options[j], MAX_OPTION_LEN, file);
+            fgets((*questions)[i].options[j], MAX_OPTION_LEN, file);
         }
 
         char option[5];
         fgets(option, 5, file);
-        questions[i].correct_option = option[0];
+        (*questions)[i].correct_option = option[0];
 
         char timeout[10];
         fgets(timeout, 10, file);
-        questions[i].timeout = atoi(timeout);
+        (*questions)[i].timeout = atoi(timeout);
 
         char prize_money[10];
         fgets(prize_money, 10, file);
-        questions[i].prize_money = atoi(prize_money);
+        (*questions)[i].prize_money = atoi(prize_money);
     }
 
     fclose(file);
+    return no_of_questions;
+}
+
+void print_formatted_question(Question question) {
+    printf("\n%s", question.text);
+    for (int i = 0; i < 4; i++) {
+        printf("%c. %s", 'A' + i, question.options[i]);
+    }
+    printf("Correct Option: %c\n", question.correct_option);
+    printf("Timeout: %d seconds\n", question.timeout);
+    printf("Prize Money: Rs.%d\n", question.prize_money);
 }
 
 int main() {
     Question* questions;
-    read_questions("questions.txt", questions);
+    int no_of_questions = read_questions("questions.txt", &questions);
+
+    for (int i = 0; i < no_of_questions; i++) {
+        print_formatted_question(questions[i]);
+    }
 
     return 0;
 }
